e8508f48ea399f1db10e5a8092557c6b107a9c5b
apayne28 <ashleypayne@Ashleys-MacBook-Pro.local>
2020-05-12 12:43:50 -0400

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ tests/shortcuts/test_make_toast.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from django.shortcuts import make_toast
+from django.test import SimpleTestCase
+
+
+class MakeToastTests(SimpleTestCase):
+    def test_make_toast(self):
+        self.assertEqual(make_toast(), 'toast')
